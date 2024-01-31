ef3e2aea38129f88bb1c7187587ad4b30903cf5d
lucindanora <144958544+lucindanora@users.noreply.github.com>
2024-01-30 19:44:30 -0600

    updated heatmap

--- Docs/static/js/app.js
+++ Docs/static/js/app.js
@@ -2,7 +2,7 @@
 const capsules = "/api/v1.0/autotheft_tb";
 
 // Fetch the JSON data and create the heatmap
-d3.json(capsules).then(function(data) {
+d3.json(capsules).then(function (data) {
     // Set up Leaflet map
     var map = L.map('map').setView([44.9778, -93.2650], 12);
 
@@ -12,12 +12,28 @@ d3.json(capsules).then(function(data) {
 
     // Normalize the counts to get an incidence rate per neighborhood
     var heatData = data.map(point => [point.latitude, point.longitude]);
-    
-    console.log(data[0])
+
+    // Create MarkerClusterGroup
+    var markers = L.markerClusterGroup();
+
+    // Create markers and add to MarkerClusterGroup
+    heatData.forEach(function (point) {
+        var marker = L.marker(new L.LatLng(point[0], point[1]));
+        markers.addLayer(marker);
+    });
+
+    // Add MarkerClusterGroup to the map
+    map.addLayer(markers);
 
     // Create and add the heatmap layer
-    var heat = L.heatLayer(heatData, { radius: 20 }).addTo(map);
+    var heat = L.heatLayer(heatData, {
+        radius: 10,
+        opacity: 0.7,
+        blur: 15,
+        maxZoom: 15
+    }).addTo(map);
 });
+
 
 
 
