6427f5e47388a3978587548f40cce50afc840e5d
Mebrahtu-2012 <144969523+Mebrahtu-2012@users.noreply.github.com>
2024-02-05 10:28:26 -0600

    sync changes

--- Docs/router.py
+++ Docs/router.py
@@ -1,14 +1,36 @@
 import numpy as np
+
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-from flask import Flask, jsonify, render_template
 import json
+from flask import Flask, jsonify, render_template
+
 
 #################################################
 # Database Setup
 #################################################
+engine = create_engine("sqlite:///autotheft.db")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(autoload_with=engine)
+
+# Save reference to the table
+AutoTheftTable = Base.classes.autotheft_tb
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
 engine = create_engine("sqlite:///autotheft.db")
 
 # Reflect an existing database into a new model
@@ -60,7 +82,7 @@ def neighborhood_areas():
     # Create our session (link) from Python to the DB
     with open("Resources\Minneapolis_Neighborhoods.geojson") as f:
         d = json.load(f)
-        return d
+        return d     
 
 @app.route("/api/v1.0/autotheft_tb")
 def autotheft_tb():
@@ -87,9 +109,40 @@ def autotheft_tb():
     finally:
         session.close()
 
+@app.route("/api/v1.0/precinct")
+def precincts():
+    session = Session(engine)
+    try:
+        results = session.query(AutoTheftTable.precinct).distinct().all()
+        all_precincts = [result[0] for result in results if result[0] not in [None, "UI", "0"]]
+        return jsonify(all_precincts)
+    finally:
+        session.close()
+
+# New route for offenses by precinct
+@app.route("/api/v1.0/offenses/<precinct>")
+def offenses_by_precinct(precinct):
+    session = Session(engine)
+    try:
+        # Query offense data for the specified precinct
+        results = session.query(AutoTheftTable).filter(AutoTheftTable.precinct == precinct).all()
+
+        # Convert query results to a list of dictionaries
+        offenses = []
+        for row in results:
+            offense_dict = {
+                "offense": row.offense,
+                "reportedDate": row.reportedDate,
+                "neighborhood": row.neighborhood
+            }
+            offenses.append(offense_dict)
+
+        return jsonify(offenses)
+    finally:
+        session.close()
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-
 
 
